1b7a8c91f4941a9525b6e5011c3f93fa19300b74
Will Kibel <56790114+willkbl@users.noreply.github.com>
2023-09-30 16:00:15 -0400

    bullets bounce off of walls now (and each other)

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float life = 3;
+    private Rigidbody rb;
+    private int maxRicochets = 3;
+    private int currentRicochets = 0;
+    private Vector3 lastVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Awake()
     {
         Destroy(gameObject, life);
     }
 
+    void LateUpdate()
+    {
+        lastVelocity = rb.velocity;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        Destroy(collision.gameObject);
-        Destroy(gameObject);
+        //Destroy(collision.gameObject);
+        //Destroy(gameObject);
+
+        if (currentRicochets < maxRicochets)
+        {
+            Vector3 reflectionDirection = Vector3.Reflect(lastVelocity, collision.contacts[0].normal);
+
+            rb.velocity = reflectionDirection;
+
+            currentRicochets++;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
