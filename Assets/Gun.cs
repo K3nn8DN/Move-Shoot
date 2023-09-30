9e1da55155008f68da79933b2fbbe1ab6a3ec4af
Will Kibel <56790114+willkbl@users.noreply.github.com>
2023-09-30 16:31:44 -0400

    added (variable) delay between shots

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -7,13 +7,23 @@ public class Gun : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
+    private bool canShoot = true;
+    public float fireRate = 0.5f;
+    private float nextFireTime = 0f;
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && canShoot)
         {
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            canShoot = false;
+            nextFireTime = Time.time + fireRate;
+        }
+
+        if (!canShoot && Time.time >= nextFireTime)
+        {
+            canShoot = true;
         }
     }
 }
