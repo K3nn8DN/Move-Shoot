a47382075c8cf340f2489fbf626c9cea0a18da71
Will Kibel <56790114+willkbl@users.noreply.github.com>
2023-09-30 20:03:26 -0400

    gun makes noise

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
@@ -12,21 +13,45 @@ public class Gun : MonoBehaviour
     private float nextFireTime = 0f;
     public static float damage = 1;
     public static bool isGunCharged = false;
+    public static float numCharges;
+
+    private AudioSource audioSource;
+    public float minPitch = 0.75f;
+    public float maxPitch = 1.25f;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void ChargeGun()
     {
         isGunCharged = true;
     }
 
+    public void ShootGun(InputAction.CallbackContext context)
+    {
+        if (canShoot && context.performed)
+        { 
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            if (audioSource != null && audioSource.clip != null)
+            {
+                audioSource.Play();
+            }
+            canShoot = false;
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) && canShoot)
+        if (audioSource.isPlaying)
         {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
-            canShoot = false;
-            nextFireTime = Time.time + fireRate;
+            float randomPitch = Random.Range(minPitch, maxPitch);
+
+            audioSource.pitch = randomPitch;
         }
 
         if (!canShoot && Time.time >= nextFireTime)
